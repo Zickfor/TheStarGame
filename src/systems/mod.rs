1a00fc8126ebab2f626c6aefb8a63b07bce6e90c
Zickfor <megahouse243@gmail.com>
2023-08-08 16:04:58 +0300

    Some code cleanup

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,19 +1,4 @@
-use std::collections::HashMap;
-use hecs::{Entity, World};
-use macroquad::prelude::Texture2D;
-
 pub mod startup;
 pub mod physics;
 pub mod render;
 pub mod controls;
-
-pub fn systems_startup(world: &mut World) -> Entity {
-    startup::create_asteroids(world, 100);
-    startup::create_ship(world)
-}
-
-pub fn systems_cycled(world: &mut World, textures: &HashMap<String, Texture2D>) {
-    controls::handle_movement_controls(world);
-    physics::system_motion(world);
-    render::texture_drawer(world, textures);
-}
